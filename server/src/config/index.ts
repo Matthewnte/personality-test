16d55f71584c692392fce73473b9036c5b123519
Matthew Nte <nte@grace.health>
2022-07-21 18:05:51 +0100

    setup docker

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,4 +2,5 @@ import 'dotenv/config';
 
 export default {
   port: process.env.PORT || 3000,
+  opt: process.env.OPT || './opt/logs',
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import app from './app';
 import config from './config';
+import logger from './config/winston';
 
-app.listen(3333, () => {
-  console.log(`🚀 Server started on port ${config.port}!`);
+app.listen(config.port, () => {
+  logger.log({
+    message: `Server started on port ${config.port}!`,
+    level: 'info',
+  });
 });
