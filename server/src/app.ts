927786fc8a41f06c33bc48f3d29b349c9e508886
Matthew Nte <nte@grace.health>
2022-07-21 17:12:55 +0100

    add health check

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import cors from 'cors';
-import express, { Request } from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors<Request>());
+
+app.get('/health', (_req, res: Response) => res.json({ status: 'up' }));
 
 export default app;
